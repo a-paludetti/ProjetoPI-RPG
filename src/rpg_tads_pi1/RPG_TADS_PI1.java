5e4a0e545596caeb32ed590345136f05676e7c82
Wynter <Wynter@192.168.100.115>
2017-05-30 19:07:36 -0300

    salas 5 e 6 redux

--- src/rpg_tads_pi1/RPG_TADS_PI1.java
+++ src/rpg_tads_pi1/RPG_TADS_PI1.java
@@ -115,7 +115,6 @@ public class RPG_TADS_PI1 {
 
     public static int quarto2(String N) {
         int HP, quarto = 0;
-        String HP1;
         System.out.println("Na sua frente se extende um corredor escuro,"
                 + " você consegue ver 3 portas a sua esquerda, uma claramente destruída."
                 + "\nAs outras duas estão intáctas; ao final uma escada,"
@@ -208,6 +207,8 @@ public class RPG_TADS_PI1 {
                 } else {
                     quarto2(palavra);
                 }
+            } else {
+                quarto2(palavra);
             }
 
             return escolha4;
@@ -233,7 +234,7 @@ public class RPG_TADS_PI1 {
                     if (escolha1.contains("1")) {
                         System.out.println("Na parede as letras"
                                 + "\n - - Q - - - E - T A"
-                                + "\n você começa a preencher as letras...");
+                                + "\nVocê começa a preencher as letras...");
                         System.out.println("");
                         palavra = "ARQUITETA";
                         jogo = jogoDaForca01(palavra);
@@ -272,25 +273,38 @@ public class RPG_TADS_PI1 {
     }
 
     public static String quarto5(int N) {
-        String status = "";
+        String escolha01, escolha3 = "", escolha4 = "", status = "";
+        int HP;
         System.out.println("((sem batalha - escada indo para baixo))");
         System.out.println("((desci~~ida talvez uma sombra no fundo???");
         System.out.println("((algum modo do jogador desenhar uma fechadura no papel e colocar na porta))");
-        status = entrada.nextLine();
-        System.out.println("((porta fecha/quebra/whatever e jogador entra no piso 2 - quarto 6.))");
+        escolha01 = entrada.nextLine();
+        if (escolha01.contains("1") || escolha01.contains("2")) {
+            System.out.println("((porta abre + tea tray.))");
+            System.out.println("((tomar chá?"
+                    + "\n> 1 - SIM"
+                    + "\n> 2 - NÃO))");
+            escolha3 = entrada.nextLine();
+            System.out.println("");
+            if (escolha3.contains("1")) {
+                System.out.println("HP + 10");
+                escolha4 = "10";
+                HP(escolha4);
+                System.out.println("");
+                quarto2(escolha4);
+            } else {
+                System.out.println("((sai do quarto))");
+                escolha4 = "";
+                quarto2(escolha4);
+            }
+        }
         return status;
     }
 
     public static int quarto6(String N) {
         int HP, quarto = 0;
-        String HP1;
-        System.out.println("Na sua frente se extende um corredor escuro,"
-                + " você consegue ver 3 portas a sua esquerda, uma claramente destruída."
-                + "\nAs outras duas estão intáctas; ao final uma escada,"
-                + " você consegue notar um vulto estranho aguardando em frente."
-                + "\n> 1 - tentar a primeira porta."
-                + "\n> 2 - tentar a segunda porta."
-                + "\n> 3 - ir em direção a escada.");
+        System.out.println("((porta fecha com um PAN! não é possível voltar."
+                + "+ 3 portas, todas abertas :D");
         String escolha = entrada.nextLine();
         System.out.println("");
 
@@ -298,20 +312,91 @@ public class RPG_TADS_PI1 {
 
             case "1":
                 quarto = 1;
-                quarto3(quarto);
+                quarto7(quarto);
                 break;
             case "2":
                 quarto = 2;
-                quarto4(quarto);
+                quarto8(quarto);
                 break;
             case "3":
                 quarto = 3;
-                quarto5(quarto);
+                quarto9(quarto);
             default:
                 System.out.println("Opção inválida");
                 break;
         }
         return quarto;
+    }
+
+    public static String quarto7(int N) {
+        String escolha1, escolha2 = "", palavra = "", escolha3 = "", escolha4 = "";
+        if (N == 1) {
+            System.out.println("A porta está trancada,  você consegue ver uma luz - "
+                    + "talvez branca? ou azul? - pela fresta."
+                    + "\n> 1 - Olhar pelo buraco da chave."
+                    + "\n> 2 - Voltar ao corredor.");
+            escolha1 = entrada.nextLine();
+            System.out.println("");
+            if (escolha1.contains("1")) {
+                System.out.println("Você vê parte da sala, apenas um quarto branco."
+                        + "\n> 1 - Tentar a segunda porta."
+                        + "\n> 2 - Voltar ao corredor.");
+                escolha2 = entrada.nextLine();
+                System.out.println("");
+                if (escolha2.contains("1")) {
+                    quarto4(2);
+                } else {
+                    quarto2(escolha4);
+                }
+                return escolha4;
+            } else {
+                quarto2(escolha4);
+            }
+        }
+        if (N == 2) {
+            System.out.println("Ao abrir a porta você se depara com um quarto vazio;"
+                    + "\nUma luz fraca illumina as paredes brancas, você não vê nenhuma porta."
+                    + "\n> 1 - Examinar o quarto."
+                    + "\n> 2 - Voltar ao corredor.");
+            escolha1 = entrada.nextLine();
+            System.out.println("");
+            if (escolha1.equals("1")) {
+                System.out.println("Ao chegar mais perto você vê que as paredes são cinzas com respingos de tinta"
+                        + "\nNa sua frente a parede parece suja com respingos pretos e cinzas, na sua direita, onde estava a fechadura, ainda mais respingos."
+                        + "\nA mulher das suas memórias, a tinta, você começa a sentir um senso de Dejà-vu..."
+                        + "\n> 1 - Jogar a tinta no chão."
+                        + "\n> 2 - Jodas a tinta na parede."
+                        + "\n> 3 - Voltar ao corredor.");
+                escolha2 = entrada.nextLine();
+                System.out.println("");
+                if (escolha2.contains("1") || escolha2.contains("2")) {
+                    System.out.println("((memória 01 - museu + tea tray.");
+                    System.out.println("((tomar chá?"
+                            + "\n> 1 - SIM"
+                            + "\n> 2 - NÃO))");
+                    escolha3 = entrada.nextLine();
+                    System.out.println("");
+                    if (escolha3.contains("1")) {
+                        System.out.println("HP + 10");
+                        escolha4 = "10";
+                        HP(escolha4);
+                        System.out.println("");
+                        quarto2(escolha4);
+                    } else {
+                        System.out.println("((sai do quarto))");
+                        escolha4 = "";
+                        quarto2(escolha4);
+                    }
+                } else {
+                    quarto2(palavra);
+                }
+            } else {
+                quarto2(palavra);
+            }
+
+            return escolha4;
+        }
+        return escolha4;
     }
 
     public static int HP(String N) {
@@ -386,17 +471,15 @@ public class RPG_TADS_PI1 {
         if (x == 1) {
             monster = hpm.nextInt(5);
         }
-
+        return monster;
     }
 
     public static void main(String[] args) {
         // TODO code application logic here
-        int x, N1;
-        String N, N2;
+        int x, N1, N2;
+        String N, N3;
         N = quarto1();
-        N1 = quarto2(N);
-        quarto3(N1);
-        quarto6;
+        N1 = quarto6(N);
 
     }
 
